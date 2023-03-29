39a721749942bf59905ec32b04d74563f06cfbdd
laurabjn <92058813+laurabjn@users.noreply.github.com>
2023-03-29 14:45:44 +0200

    feat: 🎸 home page and carousel component (begin)

---
+++ src/views/Home/HomePage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { CarouselBook } from './components/CarouselBook';
+import styles from './HomePage.module.scss';
+
+const HomePage = () => {
+    const { t } = useTranslation();
+    return (
+        <div className={styles.container}>
+            <div className={styles['last-release']}>
+                {t('home.lastRelease')}
+                <CarouselBook itemsTypes='release' />
+            </div>
+            <div className={styles.popular}>
+                {t('home.popular')}
+                <CarouselBook itemsTypes='popular' />
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
