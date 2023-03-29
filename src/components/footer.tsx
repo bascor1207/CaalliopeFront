ca07bb7ef2a41c492edd62a0461f72b19d162642
tclauzure <theo.clauzure@outlook.fr>
2023-03-29 16:13:38 +0200

    Base de la structure pour le Header

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="toContact">Nous contacter</div>
+        </footer>
+    );
+}
